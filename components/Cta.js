43445056d0c027b9fdafa1ae1e190ad7814a4f18
Shikha Kapoor <shikhakapoor@Shikhas-iMac.local>
2025-03-12 22:54:28 +0530

    update cta

--- components/Cta.js
+++ components/Cta.js
@@ -5,14 +5,14 @@ const Cta = () => {
     <section className="cta relative w-full ">
      <div className="container">
         <div className="col-wrap flex gap-4 items-center bg-orange border-orange border-1 p-[100px] rounded-2xl ">
-            <div className="col-span w-50%">
+            <div className="col-span w-[50%]">
               <h5 className="text-lg text-white uppercase mb-[12px]">NEWSLETTER</h5>
               <h2 className="text-5xl font-semibold text-white">Subscribe our newsletter.</h2>
               <div className="btn-wrap mt-[60px]">
               <a className="border-1 border-white bg-white rounded-full py-[20px] px-[36px] text-xl text-orange" href="#">Learn More</a>
               </div>
            </div>
-           <div className="image-wrap w-50% absolute right-0  xxs:max-md:hidden">
+           <div className="image-wrap w-[50%] absolute right-0  xxs:max-md:hidden">
               <Image
                 src="/image/soup.svg"
                 alt="cta"
