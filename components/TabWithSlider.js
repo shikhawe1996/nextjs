94f23423d3a18e950b61816600963a5c82debbe3
Shikha Kapoor <shikhakapoor@Shikhas-iMac.local>
2025-03-13 19:31:21 +0530

    remove error

--- components/TabWithSlider.js
+++ components/TabWithSlider.js
@@ -4,17 +4,18 @@ import React from 'react';
 export const TabPage = () => (
     <Tabs>
         <Tab title="Company">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed quia consequuntur magni dolores eos qui ratione voluptatem
         </Tab>
         <Tab title="Infomation">
             Resource Library
-            Find documents, videos and webinars here to enrich your discovery of our solutions. To find the resource you're interested filter by resource type, product, solution or a key word search.
+            Find documents, videos and webinars here to enrich your discovery of our solutions. To find the resource you&apos;re interested filter by resource type, product, solution or a key word search.
         </Tab>
-        <Tab title="Hotal Owner">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
+        <Tab title="Hotel Owner">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed quia consequuntur magni dolores eos qui ratione voluptatem
         </Tab>
     </Tabs>
 );
+
 
 /* Tab logic */
 
@@ -46,7 +47,7 @@ function Tabs({ children }) {
                     {child?.props.title}
                 </div>
             ))}
-            <section className=''>{newChildren}</section>
+            <section className>{newChildren}</section>
 
         </nav>
     );
